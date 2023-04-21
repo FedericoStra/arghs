03c5ebe0e7c2b42c48c24bd0ea50b5cdb385c8ac
Federico Stra <stra.federico@gmail.com>
2023-04-21 11:06:23 +0200

    Use `traverse_` instead of `mapM_`

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,13 @@
 module Main where
 
 import Arghs
+import Data.Foldable (traverse_)
 
 main :: IO ()
 main = do
   args <- getAllArgs
   let width = lenToWidth $ length args
-  mapM_ (printArg width) (enumerate args)
+  traverse_ (printArg width) (enumerate args)
   where
     enumerate :: [a] -> [(Int, a)]
     enumerate = zip [0 ..]
